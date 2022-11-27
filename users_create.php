a978187b6ac3ef85f747684a29d98496bb480bc9
jeremyhayotte <jeremy.hayotte@etu.unilim.fr>
2022-11-28 00:19:21 +0100

    Ajout des relations dans le script

--- users_create.php
+++ users_create.php
@@ -1,11 +1,23 @@
 <?php
 
 use App\Controllers\UsersController;
+use App\Services\CarsService;
+use App\Services\BookingsService;
+use App\Services\CarpoolAdService;
 
 require __DIR__ . '/vendor/autoload.php';
 
 $controller = new UsersController();
 echo $controller->createUser();
+
+$carsService = new CarsService();
+$cars = $carsService->getCars();
+
+$bookingsService = new BookingsService();
+$bookings = $bookingsService->getBookings();
+
+$carpoolAdService = new CarpoolAdService();
+$carpoolads = $carpoolAdService->getCarpoolAds();
 ?>
 
 <p>Création d'un utilisateur</p>
@@ -22,5 +34,25 @@ echo $controller->createUser();
     <label for="birthday">Date d'anniversaire au format dd-mm-yyyy :</label>
     <input type="text" name="birthday">
     <br />
+    <label for="cars">Voiture(s) :</label>
+    <?php foreach ($cars as $car): ?>
+        <?php $carProperties = $car->getLicensePlate() . ' ' . $car->getColor() . ' ' . $car->getCost() . ' ' . $car->getNbSeat(); ?>
+        <input type="checkbox" name="cars[]" value="<?php echo $car->getId(); ?>"><?php echo $carProperties; ?>
+        <br />
+    <?php endforeach; ?>
+    <br />
+    <label for="bookings">Réservation(s) :</label>
+    <?php foreach ($bookings as $booking): ?>
+        <?php $bookingName = $booking->getDay() . ' ' . $car->getHorary() . ' ' . $car->getNumber(); ?>
+        <input type="checkbox" name="bookings[]" value="<?php echo $booking->getId(); ?>"><?php echo $bookingName; ?>
+        <br />
+    <?php endforeach; ?>
+    <br />
+    <label for="carpoolads">Annonce(s) :</label>
+    <?php foreach ($carpoolads as $carpoolad): ?>
+        <?php $carpoolAdName = $carpoolad->getStartPlace() . ' ' . $carpoolad->getDestination() . ' ' . $carpoolad->getDepartureTime() . ' ' . $carpoolad->getPrice(); ?>
+        <input type="checkbox" name="carpoolads[]" value="<?php echo $carpoolad->getId(); ?>"><?php echo $carpoolAdName; ?>
+        <br />
+    <?php endforeach; ?>
     <input type="submit" value="Créer un utilisateur">
 </form>
