8b339255ae01df9c461a923afcba4df5b336ac7d
jeremyhayotte <jeremy.hayotte@etu.unilim.fr>
2022-11-28 00:16:20 +0100

    Ajout des relations dans users_create.php

---
+++ htdocs/index.php
@@ -0,0 +1,13 @@
+<DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>Accueil</title>
+        <meta charset="UTF-8"/>
+    </head>
+    <body>
+        <a href="./users_forms.php" >Lien vers formulaires utilisateur</a>
+        <a href="./cars_forms.php">Lien vers formulaires voiture</a>
+        <a href="./booking_forms">Lien vers formulaires réservation</a>
+        <a href="./carpoolingads">Lien vers formulaires annonce</a>
+    </body>
+</html>
