d4cf5f9a57635e3c669be182879f533c671b8581
jeremyhayotte <jeremy.hayotte@etu.unilim.fr>
2022-11-28 00:31:37 +0100

    Ajout des relations annonces->voitures et annonces->reservation

--- carpool_ad_create.php
+++ carpool_ad_create.php
@@ -1,11 +1,19 @@
 <?php
 
 use App\Controllers\CarpoolAdController;
+use App\Services\CarsService;
+use App\Services\BookingsService;
 
 require __DIR__ . '/vendor/autoload.php';
 
 $controller = new CarpoolAdController();
 echo $controller->createCarpoolAd();
+
+$carsService = new CarsService();
+$cars = $carsService->getCars();
+
+$bookingsService = new BookingsService();
+$bookings = $bookingsService->getBookings();
 ?>
 
 <p>Création d'une annonce</p>
@@ -22,6 +30,18 @@ echo $controller->createCarpoolAd();
     <label for="price">Prix :</label>
     <input type="text" name="price">
     <br />
-    
+    <label for="cars">Voiture(s) :</label>
+    <?php foreach ($cars as $car): ?>
+        <?php $carProperties = $car->getLicensePlate() . ' ' . $car->getColor() . ' ' . $car->getCost() . ' ' . $car->getNbSeat(); ?>
+        <input type="checkbox" name="cars[]" value="<?php echo $car->getId(); ?>"><?php echo $carProperties; ?>
+        <br />
+    <?php endforeach; ?>
+    <br />
+    <label for="bookings">Réservation(s) :</label>
+    <?php foreach ($bookings as $booking): ?>
+        <?php $bookingName = $booking->getDay() . ' ' . $car->getHorary() . ' ' . $car->getNumber(); ?>
+        <input type="checkbox" name="bookings[]" value="<?php echo $booking->getId(); ?>"><?php echo $bookingName; ?>
+        <br />
+    <?php endforeach; ?>
     <input type="submit" value="Créer une annonce">
 </form>
